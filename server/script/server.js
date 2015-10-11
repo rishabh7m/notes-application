5ca6dfa46a1f97bb048c0116f802a05748a012f2
rishabh7m <rishabh.makhija07@gmail.com>
2015-10-12 01:15:19 +0530

    download_pdf_incomplete

--- server/script/server.js
+++ server/script/server.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var multer = require('multer');
 var mongoClient = require('mongodb').MongoClient;
 var upload = multer({dest: '/home/rishabh/Notes/notes-application/server/data/common'});
+var objectId = require('mongodb').ObjectId;
 
 var commonCollection;
 
 //	SEMESTER PATHS
-
 var sem_1 = '/home/rishabh/Notes/notes-application/server/data/1';
 var sem_2 = '/home/rishabh/Notes/notes-application/server/data/2';
 var sem_3 = '/home/rishabh/Notes/notes-application/server/data/3';
@@ -20,7 +20,6 @@ var sem_8 = '/home/rishabh/Notes/notes-application/server/data/8';
 
 
 //	CONNECTING TO DATABASE
-
 mongoClient.connect("mongodb://localhost:27017/notes", function(err, db) {
 	if(!err) {
 		console.log('Database Connected...');
@@ -32,8 +31,8 @@ mongoClient.connect("mongodb://localhost:27017/notes", function(err, db) {
 	}
 });
 
-//	UPLOAD PDF TO SERVER
 
+//	UPLOAD PDF TO SERVER
 app.post('/api/notes/upload/:sem/:course/:teacher',upload.single('pdf'), function(req, res, next) {
 	var sem = req.params.sem;
 	var course = req.params.course;
@@ -41,33 +40,6 @@ app.post('/api/notes/upload/:sem/:course/:teacher',upload.single('pdf'), functio
 	var oldPath = req.file.path;
 	var semPath, filePath, dbValue;
 
-	/*switch(sem) {
-		case 1 :
-			semPath = sem_1;
-			console.log('Sem 1 ' + sem_1);
-			break;
-		case 2 :
-			semPath = sem_2;
-			break;
-		case 3 :
-			semPath = sem_3;
-			break;
-		case 4 :
-			semPath = sem_4;
-			break;
-		case 5 :
-			semPath = sem_5;
-			break;
-		case 6 :
-			semPath = sem_6;
-			break;
-		case 7 :
-			semPath = sem_7;
-			break;
-		case 8 :
-			semPath = sem_8;
-			break;
-	}*/
 	if(sem == 1) {
 		semPath = sem_1;
 	}
@@ -108,6 +80,44 @@ app.post('/api/notes/upload/:sem/:course/:teacher',upload.single('pdf'), functio
 			};
 			commonCollection.insert(dbValue);
 			res.send('success');
+		}
+	});
+});
+
+
+//	DOWNLOAD PDF FROM SERVER
+app.get('/api/notes/:id', function(req, res) {
+	var id = req.params.id;
+	var idQuery = 'ObjectId(" + id + ")';
+	var one = 'ObjectId("';
+	var two = one + id;
+	var three = two + '")'
+	//var _id = '_id';
+	var rish = 'rishabh';
+
+	var q1 = '{_id: ';
+	var q2 = q1 + three;
+	var q3 = q2 + '}'
+
+	var newid = new objectId(id);
+
+	/*var updateValue = {};
+	updateValue[_id] = three;*/
+
+	/*commonCollection.find({"_id": three}, {location: 1, _id: 0}).toArray(function(err, results) {
+		if(!err) {
+			console.log(results);
+		}
+		else {
+			res.send("error");
+		}
+	});*/
+	commonCollection.findOne({_id: newid}, {location: 1, _id: 0}, function(err, doc) {
+		if(!err) {
+			console.log(doc);
+		}
+		else {
+			console.log("error");
 		}
 	});
 });
